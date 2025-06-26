56b343ebb3df463c61e4a2b2a68fc72116ab6da5
Rohan Pratik Phadke (rphadke) <rphadke@longleaf-login5.its.unc.edu>
2025-06-26 08:45:04 -0400

    DiT with transform-average-concatenate

---
+++ f5_tac/model/modules.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import difflib
+
+from f5_tts.model.backbones.dit import DiT
+from f5_tts.model.modules import DiTBlock
+
+
+class DiTBlockWithTAC(DiTBlock):
+    def __init__(self, *args, dim, **kwargs):
+        super().__init__(*args, dim=dim, **kwargs)
+        self.tac = TAC(in_channels=dim, expansion_f=3, dropout=0.1)
+    
+    def forward(self, x, t, mask=None, rope=None, spk_mask=None):
+        x = super().forward(x, t, mask=mask, rope=rope) # -> (B⋅S, T, C)
+
+        B2, T, D = x.shape
+        S = 2 # TODO: infer from speaker count
+        B = B2 // S
+
+        x_spk = x.view(B, S, T, C) # -> (B, S, T, C)
+
+        # per speaker average internally
+        x_spk = self.tac(x_spk, mask=spk_mask)
+
+        x = x_spk.view(B2, T, C)   # -> (B⋅S, T, C)
+        return x
